54113afbe5949163f474ef096cf2f766116e1037
Munkhbayar <munkhb4y4r@gmail.com>
2026-09-15 20:16:41 +0800

    added passing and failing thresholds

---
+++ script-threshold.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 30,
+  duration: '1m',
+  thresholds: {
+    http_req_duration: ['p(95)<1117'],
+    http_req_failed: ['rate<0.01'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
